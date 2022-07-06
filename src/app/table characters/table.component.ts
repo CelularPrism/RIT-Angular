e4d80e457bba32a82abf111f1d406ba09bcb7ffc
CelularPrism <46044765+CelularPrism@users.noreply.github.com>
2022-07-06 23:47:34 +0700

    Create layout component Header

---
+++ src/app/table characters/table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tableComponent',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.css']
+})
+export class TableComponent {
+  title = 'RIT';
+}
